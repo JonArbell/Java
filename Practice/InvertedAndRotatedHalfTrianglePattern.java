b2499c3de262f36f8b6e2a065ed50c8eb277c40a
Jon Arbell De Ocampo <138678861+JonArbell@users.noreply.github.com>
2023-12-22 21:51:56 +0800

    Add files via upload

---
+++ Practice/InvertedAndRotatedHalfTrianglePattern.java
@@ -0,0 +1,32 @@
+public class InvertedAndRotatedHalfTrianglePattern {
+
+    public static void main(String[] args) {
+
+        /*
+        * print the pattern
+                   *
+                 * *
+               * * *
+             * * * *
+           * * * * *
+        *
+        */
+
+        int row = 5;
+        int sizeStar = 1;
+        for(int i = 0; i < row; i++){
+
+            for(int k = row; k-i > 0; k--){
+                System.out.print("  ");
+            }
+            for(int j =0;j<sizeStar;j++){
+                System.out.print("* ");
+            }
+            sizeStar++;
+            System.out.println();
+        }
+
+
+    }
+
+}
